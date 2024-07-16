5a47131ebd53b429ac8b5ee741ae9de6425c27d0
John Kokar <princedibloni4@gmail.com>
2024-07-16 12:49:29 -0700

    freaaa

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -64,7 +64,7 @@ export default function DashBoard({ navigation }) {
   };
 
   const expirationService = async () => {
-    setLoged(false);
+    setLoged(true);
 
     /*const date = new Date();
     const currentMonth = date.getMonth();
